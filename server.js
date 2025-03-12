d8aacc8887edaf038ced2010fb686998ba5e3dd5
Jeff <sutin81056@gmail.com>
2025-03-13 04:10:28 +0800

    ver 1.0.0

---
+++ server.js
@@ -0,0 +1,29 @@
+const io = require("socket.io")(5000, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+let users = []; // 存儲在線使用者 [{ id, name }]
+
+io.on("connection", (socket) => {
+  console.log("新用戶連線:", socket.id);
+
+  // 接收前端發送的用戶名稱
+  socket.on("newUser", (name) => {
+    users.push({ id: socket.id, name });
+    io.emit("updateUsers", users); // 更新所有用戶的在線列表
+  });
+
+  // 監聽新歌曲事件
+  socket.on("newSong", (data) => {
+    io.emit("newSong", data);
+  });
+
+  // 當用戶斷線時移除
+  socket.on("disconnect", () => {
+    users = users.filter((user) => user.id !== socket.id);
+    io.emit("updateUsers", users);
+  });
+});
