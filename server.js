7e94eb3be900d546011eeca1ca1cb793d9c038ff
Jeff <sutin81056@gmail.com>
2025-03-14 02:54:20 +0800

    修正 server.js中server變數

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ const server = http.createServer(app); // 讓 Express & WebSocket 共享 server
 
 const io = socketIo(server, {
   cors: {
-    origin: process.env.FRONTEND_URL || "*", // 設定允許的前端網址
+    origin:
+      process.env.FRONTEND_URL ||
+      "http://song-guessr-frontend-git-master-sutin81056s-projects.vercel.app", // 設定允許的前端網址
     methods: ["GET", "POST"],
   },
 });
@@ -36,7 +38,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
