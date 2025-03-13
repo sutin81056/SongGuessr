11c3f6b7bc08efe4d17b263befb7cfc1e4c0fc66
Jeff <sutin81056@gmail.com>
2025-03-14 03:20:09 +0800

    修正 server.js中server變數

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
